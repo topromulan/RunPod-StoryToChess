d809bfbef70de5b56a48426705674ec6af44780f
Dale Anderson <lime.dale@gmail.com>
2024-12-18 17:31:35 -0800

    support for /runpod-volume/ollama-models

--- handler_container/chess_analogy_service.sh
+++ handler_container/chess_analogy_service.sh
@@ -4,6 +4,9 @@ echo CHESS ANALOGIES STORY SERVICE
 echo -n 2024 by Dale Anderson "<lime.dale"
 echo -e "@gmail.com>\n"
 
+NETWORK_DIR=/runpod-volume/ollama-models
+[ -d $NETWORK_DIR ] && export OLLAMA_MODELS=$NETWORK_DIR || echo Network volume not found
+
 echo -- - Starting Ollama service
 nohup ollama serve 2>&1 | tee /var/log/ollama_serve.out &
 sleep 5
